9195b689375d65ba73c03abe3583264b8d826965
qiujiaaa <ooiqiujia@hotmail.com>
2021-09-07 13:00:27 +0800

    set up methods for panel and classes

---
+++ src/GamePanel.java
@@ -0,0 +1,58 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel() {
+
+    }
+
+    public void startGamee() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            
+        }
+
+    }
+}
+
